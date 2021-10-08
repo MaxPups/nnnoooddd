23503303fd4f629ffd5bc1d3e21f2686fc3929fb
Max <Max_pups@bk.ru>
2021-10-08 16:30:14 +0300

    req.url

--- index.js
+++ index.js
@@ -4,9 +4,22 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+
+
+    // console.log(req.url);
+        let result;
+        if(req.url ==='/home') result = `<h2>Home</h2>`;
+        if(req.url ==='/about') result = `<h2>About</h2>`;
+        if(req.url ==='/') result = `<h1>Main head page</h1>`
+
+
+        
+
+
+
+    res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end(result);
 });
 
 server.listen(port, hostname, () => {
